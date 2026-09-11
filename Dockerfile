65fa96d9b63c4a17936f263e14c5de451f48e5e1
BocanceaMiruna <bmiruna2015@gmail.com>
2026-09-11 03:21:53 +0300

    changes

--- Dockerfile
+++ Dockerfile
@@ -4,5 +4,6 @@ COPY requirements.txt .
 RUN pip install --no-cache-dir -r requirements.txt gunicorn opencv-python-headless flask
 COPY . .
 EXPOSE 5000
+RUN apt-get update && apt-get install -y ffmpeg libsm6 libxext6
 CMD ["gunicorn", "-w", "2", "-b", "0.0.0.0:5000", "app:app"]
 RUN pip install --no-cache-dir -r requirements.txt
--- app.py
+++ app.py
@@ -1,6 +1,7 @@
 import os
 import json
-from flask import Flask,jsonify,render_template,send_from_directory
+import cv2
+from flask import Flask,jsonify,render_template,send_from_directory,Response
 from storage import get_cale_inregistrari,config
 app=Flask(__name__)
 @app.route("/")
@@ -49,5 +50,40 @@ def get_cameras():
         })
         
     return jsonify(camere_list)
+def genereaza_cadre(url_stream):
+    os.environ["OPENCV_FFMPEG_CAPTURE_OPTIONS"] = "rtsp_transport;tcp"
+    cap = cv2.VideoCapture(url_stream, cv2.CAP_FFMPEG)
+    
+    if not cap.isOpened():
+        print("-> EROARE: OpenCV nu a putut deschide deloc streamul!", flush=True)
+        return
+
+    print("-> SUCCES: Stream-ul a fost deschis, încerc să citesc primul cadru...", flush=True)
+    
+    success, frame = cap.read()
+    if not success:
+        print("-> EROARE: S-a deschis conexiunea, dar cap.read() a returnat False (cadru gol)!", flush=True)
+        cap.release()
+        return
+
+    print("-> SUCCES TOTAL: Primul cadru a fost citit cu succes!", flush=True)
+    
+    while True:
+        success, frame = cap.read()
+        if not success:
+            break
+        ret, buffer = cv2.imencode('.jpg', frame)
+        frame_bytes = buffer.tobytes()
+        yield (b'--frame\r\n'
+               b'Content-Type: image/jpeg\r\n\r\n' + frame_bytes + b'\r\n')
+@app.route("/video_feed/<id_cam>")
+def video_feed(id_cam):
+    detalii = config.get('camere', {}).get(id_cam)
+    
+    if not detalii or not detalii.get('url_stream'):
+        return "Camera nu a fost gasita sau nu are URL", 404
+        
+    url = detalii['url_stream']
+    return Response(genereaza_cadre(url), mimetype='multipart/x-mixed-replace; boundary=frame')
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000, debug=True)
